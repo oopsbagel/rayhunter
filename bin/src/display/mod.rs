14d1a532b276abbffcd21ae807dc89f33590975d
oopsbagel <oopsbagel@disroot.org>
2025-04-30 17:46:27 -0700

    feat(display): support Alcatel MW43 and headless devices
    Add a display option to blink the LEDs on the front of the Alcatel MW43.
    Add a "dummy" display option for devices that may lack a screen or LEDs.

--- bin/src/display/mod.rs
+++ bin/src/display/mod.rs
@@ -1,4 +1,15 @@
+#[cfg(not(any(feature = "alcatel", feature = "dummy")))]
 mod generic_framebuffer;
+
+#[cfg(feature = "dummy")]
+mod dummy;
+#[cfg(feature = "dummy")]
+pub use dummy::update_ui;
+
+#[cfg(feature = "alcatel")]
+mod alcatel;
+#[cfg(feature = "alcatel")]
+pub use alcatel::update_ui;
 
 #[cfg(feature = "tplink")]
 mod tplink;
@@ -6,7 +17,6 @@ mod tplink;
 mod tplink_framebuffer;
 #[cfg(feature = "tplink")]
 mod tplink_onebit;
-
 #[cfg(feature = "tplink")]
 pub use tplink::update_ui;
 
@@ -21,8 +31,19 @@ pub enum DisplayState {
     WarningDetected,
 }
 
+#[cfg(all(feature = "orbic", feature = "alcatel"))]
+compile_error!("cannot compile for many devices at once");
+
 #[cfg(all(feature = "orbic", feature = "tplink"))]
 compile_error!("cannot compile for many devices at once");
 
-#[cfg(not(any(feature = "orbic", feature = "tplink")))]
+#[cfg(all(feature = "tplink", feature = "alcatel"))]
+compile_error!("cannot compile for many devices at once");
+
+#[cfg(not(any(
+    feature = "orbic",
+    feature = "tplink",
+    feature = "alcatel",
+    feature = "dummy"
+)))]
 compile_error!("cannot compile for no device at all");
